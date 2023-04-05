21581ce0c71239c9cae545c27f2138da2132b883
석성희 <sunghee119z@naver.com>
2023-04-05 21:46:32 +0900

    DTO로 조회 추가
    DTO로 조회 추가

--- spring-data-jpa/src/main/java/com/study/springdatajpa/repository/MemberRepository.java
+++ spring-data-jpa/src/main/java/com/study/springdatajpa/repository/MemberRepository.java
@@ -1,13 +1,28 @@
 package com.study.springdatajpa.repository;
 
+import com.study.springdatajpa.dto.MemberDto;
 import com.study.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
+
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new com.study.springdatajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
--- spring-data-jpa/src/test/java/com/study/springdatajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/com/study/springdatajpa/repository/MemberRepositoryTest.java
@@ -7,11 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
-@Transactional
 @Rollback(false)
 public class MemberRepositoryTest {
     @Autowired
@@ -25,5 +27,17 @@ public class MemberRepositoryTest {
         assertTrue(findMember.getUsername().equals(member.getUsername()));
         assertTrue(findMember.equals(member)); //JPA 엔티티 동일성 보장
 
+    }
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+        List<Member> result =
+                memberJpaRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        assertTrue(result.get(0).getUsername().equals("AAA"));
+        assertTrue(result.get(0).getAge()==(20));
+        assertEquals(result.size(),1);
     }
 }
